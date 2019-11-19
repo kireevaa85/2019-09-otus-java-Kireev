8771bd0dd20edfd553e45e988b202fa1915fdaa3
kireevaa85 <kireevaa85@gmail.com>
2019-11-19 23:01:41 +0300

    Написать свой тестовый фреймворк. Исправления по замечаниям

--- hw05-annotations/src/main/java/ru/otus/hw05Annotations/myTestFramework/core/Runner.java
+++ hw05-annotations/src/main/java/ru/otus/hw05Annotations/myTestFramework/core/Runner.java
@@ -1,77 +1,67 @@
 package ru.otus.hw05Annotations.myTestFramework.core;
 
-import ru.otus.hw05Annotations.myTestFramework.annotations.*;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+
+import static ru.otus.hw05Annotations.myTestFramework.util.ReflectionHelper.fillMethodsMap;
+import static ru.otus.hw05Annotations.myTestFramework.util.ReflectionHelper.invokeMethods;
 
 public class Runner {
 
-    public static String run(String className) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+    public static final String BEFORE_ALL = "BEFORE_ALL";
+    public static final String AFTER_ALL = "AFTER_ALL";
+    public static final String BEFORE_EACH = "BEFORE_EACH";
+    public static final String AFTER_EACH = "AFTER_EACH";
+    public static final String TEST = "TEST";
+
+    public static String run(String className) throws ClassNotFoundException, NoSuchMethodException {
         StringBuilder result = new StringBuilder();
         Class<?> classTest = Class.forName(className);
         Constructor<?> defConstructor = classTest.getConstructor();
         Method[] methods = classTest.getDeclaredMethods();
 
-        Set<Method> beforeAllSet = new HashSet<>();
-        Set<Method> afterAllSet = new HashSet<>();
-        Set<Method> beforeEachSet = new HashSet<>();
-        Set<Method> afterEachSet = new HashSet<>();
-        Set<Method> testSet = new HashSet<>();
+        Map<String, Set<Method>> map = fillMethodsMap(methods);
 
-        for (Method method : methods) {
-            if (method.getDeclaredAnnotation(BeforeAll.class) != null) {
-                beforeAllSet.add(method);
-            } else if (method.getDeclaredAnnotation(AfterAll.class) != null) {
-                afterAllSet.add(method);
-            } else if (method.getDeclaredAnnotation(BeforeEach.class) != null) {
-                beforeEachSet.add(method);
-            } else if (method.getDeclaredAnnotation(AfterEach.class) != null) {
-                afterEachSet.add(method);
-            } else if (method.getDeclaredAnnotation(Test.class) != null) {
-                testSet.add(method);
-            }
-        }
-
-        validateSingleAnnotation(beforeAllSet.size(), BeforeAll.class.getSimpleName(), result);
-        validateSingleAnnotation(afterAllSet.size(), AfterAll.class.getSimpleName(), result);
-        validateSingleAnnotation(beforeEachSet.size(), BeforeEach.class.getSimpleName(), result);
-        validateSingleAnnotation(afterEachSet.size(), AfterEach.class.getSimpleName(), result);
-
-        Object instance = defConstructor.newInstance();
         int doneCount = 0;
-        int failCount = 0;
-        invokeMethods(beforeAllSet, instance);
-        for (Method method : testSet) {
-            invokeMethods(beforeEachSet, instance);
-            try {
-                method.invoke(instance);
-                result.append("\t").append(method.getName()).append(" - done.\n");
-                doneCount++;
-            } catch (InvocationTargetException e) {
-                result.append("\t").append(method.getName()).append(" - fail. ").append(e.getTargetException().getMessage()).append("\n");
-                failCount++;
+        try {
+            invokeMethods(map.get(BEFORE_ALL), null);
+            Object instance;
+            for (Method method : map.get(TEST)) {
+                instance = defConstructor.newInstance();
+                try {
+                    invokeMethods(map.get(BEFORE_EACH), instance);
+                    try {
+                        method.invoke(instance);
+                        result.append("\t").append(method.getName()).append(" - done.\n");
+                        doneCount++;
+                    } catch (InvocationTargetException e) {
+                        result.append("\t").append(method.getName()).append(" - fail. ").append(e.getTargetException().getMessage()).append("\n");
+                    }
+                } catch (Exception e) {
+                    result.append(e.getMessage()).append("\n");
+                } finally {
+                    try {
+                        invokeMethods(map.get(AFTER_EACH), instance);
+                    } catch (Exception e) {
+                        result.append(e.getMessage()).append("\n");
+                    }
+                }
             }
-            invokeMethods(afterEachSet, instance);
+        } catch (Exception e) {
+            result.append(e.getMessage()).append("\n");
+        } finally {
+            try {
+                invokeMethods(map.get(AFTER_ALL), null);
+            } catch (Exception e) {
+                result.append(e.getMessage()).append("\n");
+            }
         }
-        invokeMethods(afterAllSet, instance);
-        result.insert(0, className + ": test count " + testSet.size() + ", done " + doneCount + ", fail " + failCount + "\n");
+
+        result.insert(0, className + ": test count " + map.get(TEST).size() + ", done " + doneCount + ", fail " + (map.get(TEST).size() - doneCount) + "\n");
         return result.toString();
-    }
-
-    private static void validateSingleAnnotation(int size, String simpleName, StringBuilder result) {
-        if (size > 1) {
-            result.append("Annotation ").append(simpleName).append(" should be in a single copy!\n");
-        }
-    }
-
-    private static void invokeMethods(Set<Method> eachSet, Object instance) throws InvocationTargetException, IllegalAccessException {
-        for (Method method : eachSet) {
-            method.invoke(instance);
-        }
     }
 
 }
---
+++ hw05-annotations/src/main/java/ru/otus/hw05Annotations/myTestFramework/util/ReflectionHelper.java
@@ -0,0 +1,53 @@
+package ru.otus.hw05Annotations.myTestFramework.util;
+
+import ru.otus.hw05Annotations.myTestFramework.annotations.*;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static ru.otus.hw05Annotations.myTestFramework.core.Runner.*;
+
+public final class ReflectionHelper {
+
+    public static Map<String, Set<Method>> fillMethodsMap(Method[] methods) {
+        Map<String, Set<Method>> result = new HashMap<>();
+        result.put(BEFORE_ALL, new HashSet<>());
+        result.put(AFTER_ALL, new HashSet<>());
+        result.put(BEFORE_EACH, new HashSet<>());
+        result.put(AFTER_EACH, new HashSet<>());
+        result.put(TEST, new HashSet<>());
+        for (Method method : methods) {
+            if (method.getDeclaredAnnotation(BeforeAll.class) != null) {
+                result.get(BEFORE_ALL).add(method);
+            } else if (method.getDeclaredAnnotation(AfterAll.class) != null) {
+                result.get(AFTER_ALL).add(method);
+            } else if (method.getDeclaredAnnotation(BeforeEach.class) != null) {
+                result.get(BEFORE_EACH).add(method);
+            } else if (method.getDeclaredAnnotation(AfterEach.class) != null) {
+                result.get(AFTER_EACH).add(method);
+            } else if (method.getDeclaredAnnotation(Test.class) != null) {
+                result.get(TEST).add(method);
+            }
+        }
+        return result;
+    }
+
+    public static void invokeMethods(Set<Method> eachSet, Object instance) throws Exception {
+        StringBuilder errMessage = new StringBuilder();
+        for (Method method : eachSet) {
+            try {
+                method.invoke(instance);
+            } catch (Exception e) {
+                errMessage.append(e.getMessage()).append("\n");
+            }
+        }
+        if (errMessage.length() > 0) {
+            throw new Exception(errMessage.toString());
+        }
+    }
+
+}
