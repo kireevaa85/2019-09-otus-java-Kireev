fdec4aacbf10ead2250ba3ba4535a36e3c4c4ba9
kireevaa85 <kireevaa85@gmail.com>
2019-12-20 14:07:19 +0300

    hw09 VeloORM. Account

---
+++ hw09-VeloORM/src/main/java/ru/otus/veloorm/DbServiceOrmDemo2.java
@@ -0,0 +1,61 @@
+package ru.otus.veloorm;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.veloorm.api.dao.AccountDao;
+import ru.otus.veloorm.api.dao.UserDao;
+import ru.otus.veloorm.api.model.Account;
+import ru.otus.veloorm.api.model.User;
+import ru.otus.veloorm.api.service.DBServiceAccount;
+import ru.otus.veloorm.api.service.DBServiceAccountImpl;
+import ru.otus.veloorm.api.service.DBServiceUser;
+import ru.otus.veloorm.api.service.DBServiceUserImpl;
+import ru.otus.veloorm.h2.DataSourceH2;
+import ru.otus.veloorm.jdbc.dao.AccountDaoJdbc;
+import ru.otus.veloorm.jdbc.dao.UserDaoJdbc;
+import ru.otus.veloorm.jdbc.sessionmanager.SessionManagerJdbc;
+
+import javax.sql.DataSource;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Optional;
+
+public class DbServiceOrmDemo2 {
+    private static Logger logger = LoggerFactory.getLogger(DbServiceOrmDemo2.class);
+
+    public static void main(String[] args) throws SQLException {
+        DataSource dataSource = new DataSourceH2();
+        DbServiceOrmDemo2 dbServiceOrmDemo2 = new DbServiceOrmDemo2();
+
+        dbServiceOrmDemo2.createTable(dataSource);
+
+        SessionManagerJdbc sessionManager = new SessionManagerJdbc(dataSource);
+        AccountDao accountDao = new AccountDaoJdbc(sessionManager);
+        DBServiceAccount dbServiceAccount = new DBServiceAccountImpl(accountDao);
+
+        long id = dbServiceAccount.saveAccount(new Account(0, "dbServiceAccount", new BigDecimal(1800)));
+        Optional<Account> account = dbServiceAccount.getAccount(id);
+        System.out.println(account);
+        account.ifPresentOrElse(
+                crAccount -> logger.info("created account, type:{}", crAccount.getType()),
+                () -> logger.info("account was not created")
+        );
+
+        dbServiceAccount.updateAccount(new Account(0, "dbServiceAccountNEW", new BigDecimal(3600)));
+        account = dbServiceAccount.getAccount(id);
+        account.ifPresentOrElse(
+                crAccount -> logger.info("UPDATED account:" + crAccount),
+                () -> logger.info("account was not updated")
+        );
+    }
+
+    private void createTable(DataSource dataSource) throws SQLException {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement pst = connection.prepareStatement("create table account(no bigint(20) NOT NULL auto_increment, type varchar(255), rest number)")) {
+            pst.executeUpdate();
+        }
+        System.out.println("table created");
+    }
+}
---
+++ hw09-VeloORM/src/main/java/ru/otus/veloorm/jdbc/dao/AccountDaoJdbc.java
@@ -0,0 +1,62 @@
+package ru.otus.veloorm.jdbc.dao;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.veloorm.api.dao.AccountDao;
+import ru.otus.veloorm.api.dao.AccountDaoException;
+import ru.otus.veloorm.api.model.Account;
+import ru.otus.veloorm.api.sessionmanager.SessionManager;
+import ru.otus.veloorm.jdbc.sessionmanager.SessionManagerJdbc;
+import ru.otus.veloorm.orm.JdbcTemplate;
+import ru.otus.veloorm.orm.JdbcTemplateImpl;
+
+import java.util.Optional;
+
+public class AccountDaoJdbc implements AccountDao {
+    private static Logger logger = LoggerFactory.getLogger(AccountDaoJdbc.class);
+
+    private final SessionManagerJdbc sessionManager;
+    private final JdbcTemplate jdbcTemplate;
+
+    public AccountDaoJdbc(SessionManagerJdbc sessionManager) {
+        this.sessionManager = sessionManager;
+        this.jdbcTemplate = new JdbcTemplateImpl(sessionManager);
+    }
+
+    @Override
+    public Optional<Account> findById(long id) {
+        try {
+            var account = jdbcTemplate.load(id, Account.class);
+            return Optional.ofNullable(account);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public long saveAccount(Account account) {
+        try {
+            return jdbcTemplate.create(account);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            throw new AccountDaoException(e);
+        }
+    }
+
+    @Override
+    public void updateAccount(Account account) {
+        try {
+            jdbcTemplate.update(account);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            throw new AccountDaoException(e);
+        }
+    }
+
+    @Override
+    public SessionManager getSessionManager() {
+        return sessionManager;
+    }
+
+}
